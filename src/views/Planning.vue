37a03e523c8166a0f487d7730979806ba6e85712
murat.s <muratsamat090598@gmail.com>
2020-12-20 18:01:59 +0600

    Урок 3. CRM на VueJS. Основной layout

--- src/views/Planning.vue
+++ src/views/Planning.vue
@@ -1,33 +1,24 @@
 <template>
   <div>
-    <main class="app-content">
-      <div class="app-page">
+    <div class="page-title">
+      <h3>Планирование</h3>
+      <h4>12 212</h4>
+    </div>
 
-
-        <div>
-          <div class="page-title">
-            <h3>Планирование</h3>
-            <h4>12 212</h4>
-          </div>
-
-          <section>
-            <div>
-              <p>
-                <strong>Девушка:</strong>
-                12 122 из 14 0000
-              </p>
-              <div class="progress" >
-                <div
-                    class="determinate green"
-                    style="width:40%"
-                ></div>
-              </div>
-            </div>
-          </section>
+    <section>
+      <div>
+        <p>
+          <strong>Девушка:</strong>
+          12 122 из 14 0000
+        </p>
+        <div class="progress" >
+          <div
+              class="determinate green"
+              style="width:40%"
+          ></div>
         </div>
-
       </div>
-    </main>
+    </section>
   </div>
 </template>
 
