9c1e8cded2548115db53d37dbfa19b62a0402783
murat.s <muratsamat090598@gmail.com>
2021-01-05 15:42:23 +0600

    Урок 13. CRM на VueJS. Создание tooltip директивы

--- src/store/record.js
+++ src/store/record.js
@@ -10,6 +10,16 @@ export default {
                 commit('SET_ERROR', e)
                 throw e
             }
+        },
+        async FETCH_RECORDS({dispatch, commit}) {
+            try {
+                const uid = await dispatch('GET_UID')
+                const records = (await firebase.database().ref(`/users/${uid}/records`).once('value')).val() || {}
+                return Object.keys(records).map(key => ({...records[key], id: key}))
+            } catch (e) {
+                commit('SET_ERROR', e)
+                throw e
+            }
         }
     }
 }
